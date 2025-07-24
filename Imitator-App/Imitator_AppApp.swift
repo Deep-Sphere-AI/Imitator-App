7adadac554b7689ebe9bdfcae2b7a7d75041af64
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2025-07-24 02:16:07 -0500

    Initial Commit

---
+++ Imitator-App/Imitator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imitator_AppApp.swift
+//  Imitator-App
+//
+//  Created by Giorgio Mancusi on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Imitator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
