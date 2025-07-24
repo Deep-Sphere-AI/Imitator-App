b38c9f21a835ec8a90526aef8f651066d130a5a7
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2025-07-24 16:56:09 -0500

    Initial files

--- Imitator-App/ContentView.swift
+++ Imitator-App/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var result: String = "Loading.."
+    let model = LLMModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(result)
+                .padding()
+                .task{
+                    do {
+                        if let resourcePath = Bundle.main.resourcePath {
+                            let files = try? FileManager.default.contentsOfDirectory(atPath: resourcePath)
+                            print("Bundle resources:", files ?? [])
+                        }
+                        
+                        guard let url = Bundle.main.url(
+                            forResource: "gemma-3n-E2B-it-Q4_K_M",
+                            withExtension: "gguf",
+                        ) else {
+                            result = "❌ Model file missing"
+                            return
+                        }
+                    
+                        try await model.load(at: url.path)
+                    
+                        result = try await model.run(prompt: "What is the meaning of life?")
+                } catch {
+                    result = "Error: \(error)"
+                }
+            }
         }
-        .padding()
     }
 }
 
--- Imitator-App/Imitator_AppApp.swift
+++ Imitator-App/Imitator_AppApp.swift
@@ -6,9 +6,32 @@
 //
 
 import SwiftUI
+import llama
+
+actor LLMModel {
+    private var state: LlmState?
+    
+    @LlmActor
+    func load(at modelPath: String) async throws {
+        let params = CommonParams(seed: UInt32.random(in: UInt32.min...UInt32.max))
+        let state = try? LlmState.create(modelPath: modelPath, params: params)
+    }
+    
+    func run(prompt: String) async throws -> String {
+        guard let state  = state else {
+            throw NSError(domain: "GemmaModel", code:1,
+                          userInfo: [NSLocalizedDescriptionKey: "Model not loaded"])
+        }
+        var output = ""
+        for try await token in await state.predict(text:prompt) {
+            output += token
+        }
+        return output
+    }
+}
 
 @main
-struct Imitator_AppApp: App {
+struct Imitator_AppApp: App {    
     var body: some Scene {
         WindowGroup {
             ContentView()
