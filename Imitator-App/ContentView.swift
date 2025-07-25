2d7fab659f8850bd13d545a1f0dbf2a9c01d9261
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2025-07-24 21:22:59 -0500

    Added gemma 3n model2

--- Imitator-App/ContentView.swift
+++ Imitator-App/ContentView.swift
@@ -8,35 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var result: String = "Loading.."
-    let model = LLMModel()
+    @StateObject private var llamaState = LlamaState()
+    @State private var multiLineText = ""
     
     var body: some View {
         VStack {
-            Text(result)
+            Text(llamaState.messageLog)
                 .padding()
-                .task{
-                    do {
-                        if let resourcePath = Bundle.main.resourcePath {
-                            let files = try? FileManager.default.contentsOfDirectory(atPath: resourcePath)
-                            print("Bundle resources:", files ?? [])
-                        }
-                        
-                        guard let url = Bundle.main.url(
-                            forResource: "gemma-3n-E2B-it-Q4_K_M",
-                            withExtension: "gguf",
-                        ) else {
-                            result = "❌ Model file missing"
-                            return
-                        }
-                    
-                        try await model.load(at: url.path)
-                    
-                        result = try await model.run(prompt: "What is the meaning of life?")
-                } catch {
-                    result = "Error: \(error)"
+                .task {
+                    sendText()
                 }
-            }
+        }
+    }
+    
+    func sendText() {
+        Task {
+            await llamaState.complete(text: "What is the meaning of life?")
+            multiLineText = ""
         }
     }
 }
---
+++ Imitator-App/llama_swift.swift
@@ -0,0 +1,80 @@
+//
+//  llama_swfit.swift
+//  Imitator-App
+//
+//  Created by Giorgio Mancusi on 7/24/25.
+//
+import SwiftUI
+import Foundation
+import Combine
+
+@MainActor
+class LlamaState: ObservableObject {
+    @Published var messageLog = ""
+    
+    private var llamaContext: LlamaContext?
+    private var defaultModelUrl: URL? {
+        Bundle.main.url(
+            forResource: "gemma-3n-E2B-it-Q4_K_M",
+            withExtension: "gguf",
+        )
+    }
+    
+    init() {
+        loadDefaultModel()
+    }
+    
+    private func loadDefaultModel() {
+        do {
+            try loadModel(modelUrl: defaultModelUrl)
+        } catch {
+            messageLog += "Failed to load default model.\n"
+        }
+    }
+    
+    func loadModel(modelUrl: URL?) throws {
+        if let modelUrl {
+            messageLog += "Loading model...\n"
+            llamaContext = try LlamaContext.create_context(path: modelUrl.path)
+            messageLog += "Loaded model \(modelUrl.lastPathComponent)\n"
+        } else {
+            messageLog += "No model specified.\n"
+        }
+    }
+    
+    func complete(text: String) async {
+        guard let llamaContext else {
+            return
+        }
+        
+        await llamaContext.completion_init(text: text)
+        
+        messageLog += "\(text)"
+        
+        Task.detached {
+            while await !llamaContext.is_done {
+                let result = await llamaContext.completion_loop()
+                await MainActor.run {
+                    self.messageLog += "\(result)"
+                }
+            }
+            
+            await llamaContext.clear()
+
+            await MainActor.run {
+                self.messageLog += """
+                    Done
+                    """
+            }
+        }
+    }
+    
+    func clear() async {
+        guard let llamaContext else {
+            return
+        }
+        
+        await llamaContext.clear()
+        messageLog = ""
+    }
+}
